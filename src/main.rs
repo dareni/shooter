ddc0fe30dcb6fc94e68de4bba87671101a452600
Daren Isaacs <ikkyisaacs@gmail.com>
2024-12-10 14:12:34 +1000

    Upgrade to bevy 0.15.0

--- src/main.rs
+++ src/main.rs
@@ -49,11 +49,11 @@ fn setup(
     asset_server: Res<AssetServer>,
 ) {
     // plane
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Plane3d::default().mesh().size(100.0, 100.0)),
-        material: materials.add(Color::srgb(0.3, 0.5, 0.3)),
-        ..default()
-    });
+    commands.spawn((
+        Mesh3d(meshes.add(Plane3d::default().mesh().size(100.0, 100.0))),
+        MeshMaterial3d(materials.add(Color::srgb(0.3, 0.5, 0.3))),
+    ));
+
     // cube
     // Assign vertex colors based on vertex positions
     let mut colorful_cube = Mesh::from(Cuboid::default());
@@ -67,61 +67,38 @@ fn setup(
         colorful_cube.insert_attribute(Mesh::ATTRIBUTE_COLOR, colors);
     }
     commands
-        .spawn(PbrBundle {
-            mesh: meshes.add(colorful_cube),
-            // This is the default color, but note that vertex colors are
-            // multiplied by the base color, so you'll likely want this to be
-            // white if using vertex colors.
-            material: materials.add(Color::srgb(1., 1., 1.)),
-            transform: Transform::from_xyz(0.0, 0.5, 0.0),
-            ..default()
-        })
+        .spawn((
+            Mesh3d(meshes.add(colorful_cube)),
+            MeshMaterial3d(materials.add(Color::srgb(1., 1., 1.))),
+            Transform::from_xyz(0.0, 0.5, 0.0),
+        ))
         .insert(Cube);
 
     // Light
-    //commands.spawn(PointLightBundle {
-    //    point_light: PointLight {
-    //        intensity: 100_000_000.0,
-    //        shadows_enabled: true,
-    //        ..default()
-    //    },
-    //    transform: Transform::from_xyz(4.0, 15.0, 4.0).looking_at(Vec3::ZERO, Vec3::Y),
-    //    ..default()
-    //});
-
-    // ambient light
-    //commands.insert_resource(AmbientLight {
-    //    color: WHITE.into(),
-    //    brightness: 200.02,
-    //});
-    commands.spawn(DirectionalLightBundle {
-        directional_light: DirectionalLight {
+    commands.spawn((
+        DirectionalLight {
             illuminance: light_consts::lux::OVERCAST_DAY,
             shadows_enabled: true,
             ..default()
         },
-        transform: Transform {
+        Transform {
             translation: Vec3::new(0.0, 2.0, 0.0),
             rotation: Quat::from_rotation_x(-PI / 4.),
             ..default()
         },
-        // The default cascade config is designed to handle large scenes.
-        // As this example has a much smaller world, we can tighten the shadow
-        // bounds for better visual quality.
-        cascade_shadow_config: CascadeShadowConfigBuilder {
+        CascadeShadowConfigBuilder {
+            minimum_distance: 1.0,
             first_cascade_far_bound: 4.0,
             maximum_distance: 100.0,
             ..default()
         }
-        .into(),
-        ..default()
-    });
+        .build(),
+    ));
 
     // Camera
-    commands.spawn(Camera3dBundle {
-        //transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        transform: Transform::from_xyz(50.0, 15.0, 30.0).looking_at(Vec3::ZERO, Vec3::Y),
-        camera: Camera {
+    commands.spawn((
+        Camera3d::default(),
+        Camera {
             order: 1,
             viewport: Some(Viewport {
                 physical_size: UVec2 { x: 200, y: 200 },
@@ -130,14 +107,13 @@ fn setup(
             }),
             ..default()
         },
-        ..default()
-    });
+        Transform::from_xyz(50.0, 15.0, 30.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
 
-    commands.spawn(SceneBundle {
-        transform: Transform::from_xyz(-1.0, 4.0, 0.0),
-        scene: asset_server.load(GltfAssetLabel::Scene(0).from_asset("littleman.glb")),
-        ..default()
-    });
+    commands.spawn((
+        Transform::from_xyz(-2.0, 4.0, 0.0),
+        SceneRoot(asset_server.load(GltfAssetLabel::Scene(0).from_asset("littleman.glb"))),
+    ));
 }
 
 fn move_cube(
@@ -171,6 +147,6 @@ fn move_cube(
 
 fn rotate_on_timer(time: Res<Time>, mut query: Query<&mut Transform, With<Cube>>) {
     for mut transform in query.iter_mut() {
-        transform.rotate(Quat::from_rotation_y(time.delta_seconds()));
+        transform.rotate(Quat::from_rotation_y(time.delta_secs()));
     }
 }
